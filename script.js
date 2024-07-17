af1d5e1926a635e4f0fd9bb509fab836025d1a7e
Jose Ivan <cervantesmartinezjoseivan@gmail.com>
2024-07-16 22:25:50 -0600

    feature: se añadio un menu y se creo un archivo javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+let btn = document.querySelector('#btn');
+let sidebar = document.querySelector('.sidebar');
+
+btn.onclick = function () {
+    sidebar.classList.toggle('active');
+};
+
+
+let pf2e = document.querySelector('#pf2e');
+let box = document.querySelector('.box');
+
+pf2e.onclick = function () {
+    box.classList.toggle('active');
+};
